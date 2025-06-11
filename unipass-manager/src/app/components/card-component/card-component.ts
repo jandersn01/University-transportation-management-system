7c6a195127865fe876ffa9cc2e90bdbada043770
Maria Eduarda Vitorino <maria.vitorino@gran.com>
2025-06-11 01:27:36 -0300

    feat: adiciona card component

--- unipass-manager/src/app/components/card-component/card-component.ts
+++ unipass-manager/src/app/components/card-component/card-component.ts
@@ -1,9 +1,17 @@
-import { Component } from "@angular/core";  
+import { Component, Input } from "@angular/core";  
+
+interface Horario {
+  id: number;
+  nome: string;
+  descricao: string;
+}
 
 @Component({
     selector: 'card-component',
+    standalone: true,
     templateUrl: './card-component.html',
     styleUrls: ['./card-component.css']
 })
 export class CardComponent {
+    @Input() horario!: Horario;
 }
