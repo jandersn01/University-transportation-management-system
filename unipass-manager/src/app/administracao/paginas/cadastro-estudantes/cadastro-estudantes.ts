c5964263fc98c305018bbb6d949d79ccf66a5811
Jandersn01 <jandersonsantos030@gmail.com>
2025-06-24 20:02:49 -0300

    feature: Json-Server criado

---
+++ unipass-manager/src/app/administracao/paginas/cadastro-estudantes/cadastro-estudantes.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms'
+
+@Component({
+  selector: 'app-cadastro-estudantes',
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './cadastro-estudantes.html',
+  styleUrl: './cadastro-estudantes.css'
+})
+export class CadastroEstudantes {
+  private formerbuilder = inject(FormBuilder);
+  private estudanteService = inject(EstudanteService);
+
+  cadastroForm: FormGroup;
+  mensagem: string = '';
+
+  constructor(){
+    this.cadastroForm = this.formerbuilder.group(
+      {
+          cpf: ['', Validators.required],
+          nomeCompleto: ['', Validators.required],
+          rg: ['', Validators.required],
+          dataNascimento: ['', Validators.required],
+          universidade: ['', Validators.required],
+          campus: ['', Validators.required],
+          turno: ['', Validators.required],
+          previsaoConclusao: ['', Validators.required],
+          declaracaoVinculoUrl: ['path/declaracao.pdf', Validators.required],
+          comprovanteResidenciaUrl: ['path/residencia.pdf', Validators.required]
+
+      }
+    )
+  }
+
+  onSubmit(): void {
+    if (this.cadastroForm.valid) {
+      this.estudanteService.cadastrar(this.cadastroForm.value).subscribe({
+        next: () => {
+          this.mensagem = 'Cadastro enviado com sucesso! Aguarde a aprovação.';
+          this.cadastroForm.reset();
+        },
+        error: (err) => {
+          this.mensagem = 'Erro ao enviar cadastro. Tente novamente.';
+          console.error(err);
+        }
+      });
+    }
+  }
+}
