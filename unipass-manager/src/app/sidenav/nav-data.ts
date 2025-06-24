423808fade236882b3847b9d4b77ec354d5d33e7
Jandersn01 <jandersonsantos030@gmail.com>
2025-06-24 18:32:52 -0300

    origin

--- unipass-manager/src/app/sidenav/nav-data.ts
+++ unipass-manager/src/app/sidenav/nav-data.ts
@@ -3,33 +3,32 @@ import { RouterLink } from "@angular/router";
 export const navaBarData = [
     {
         routeLink: 'inicio',
-        icon: 'fal fa-home',
+        icon: 'fal fa-home', // Mantido: Perfeito para a página inicial.
         label: 'inicio'
-    }, 
+    },
     {
         routeLink: 'solicitacoes-estudantis',
-        icon: 'fal fa-user-graduate',
+        icon: 'fal fa-user-graduate', // Alterado: Ícone de "adicionar solicitação" ou "documento".
         label: 'Solicitações Estudantis'
-    }, 
-
+    },
     {
         routeLink: 'gerenciamento',
-        icon: 'fal fa-user-graduate',
+        icon: 'fal fa-sitemap', // Alterado: Ícone que representa estrutura e organização.
         label: 'Gerenciamento'
     },
     {
         routeLink: 'comunicacao',
-        icon: 'fal fa-user-graduate',
+        icon: 'fal fa-bullhorn', // Alterado: Ícone universal para anúncios e comunicação.
         label: 'Comunicação'
     },
     {
         routeLink: 'administracao',
-        icon: 'fal fa-user-graduate',
+        icon: 'fal fa-user-shield', // Alterado: Combina "usuário" com "segurança/privilégios de admin".
         label: 'Administração'
     },
     {
         routeLink: 'configuracoes',
-        icon: 'fal fa-user-graduate',
+        icon: 'fal fa-cog', // Alterado: Ícone universal para configurações.
         label: 'Configurações'
     },
 ];
