218c613b8d4e8e2def0eaf2c56df84b93cb86d04
Jandersn01 <jandersonsantos030@gmail.com>
2025-06-24 18:03:53 -0300

    feat: Adicionado os demais componentes ao navbar

--- unipass-manager/src/app/app.routes.ts
+++ unipass-manager/src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './inicio/inicio';
 import { SolicitacoesEstudantis } from './solicitacoes-estudantis/solicitacoes-estudantis';
+import { Gerenciamento } from './gerenciamento/gerenciamento';
+import { Comunicacao } from './comunicacao/comunicacao';
+import { Administracao } from './administracao/administracao';
+import { Configuracoes } from './configuracoes/configuracoes';
+
+
+
 export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     {path: 'inicio', component: Inicio },
-    {path: 'solicitacoes-estudantis', component: SolicitacoesEstudantis}
+    {path: 'solicitacoes-estudantis', component: SolicitacoesEstudantis},
+    {path: 'gerenciamento', component: Gerenciamento},
+    {path: 'comunicacao', component: Comunicacao},
+    {path:'administracao', component: Administracao},
+    {path: 'configuracoes', component: Configuracoes}
+
 ];
--- unipass-manager/src/app/sidenav/nav-data.ts
+++ unipass-manager/src/app/sidenav/nav-data.ts
@@ -10,5 +10,26 @@ export const navaBarData = [
         routeLink: 'solicitacoes-estudantis',
         icon: 'fal fa-user-graduate',
         label: 'Solicitações Estudantis'
-    }
+    }, 
+
+    {
+        routeLink: 'gerenciamento',
+        icon: 'fal fa-user-graduate',
+        label: 'Gerenciamento'
+    },
+    {
+        routeLink: 'comunicacao',
+        icon: 'fal fa-user-graduate',
+        label: 'Comunicação'
+    },
+    {
+        routeLink: 'administracao',
+        icon: 'fal fa-user-graduate',
+        label: 'Administração'
+    },
+    {
+        routeLink: 'configuracoes',
+        icon: 'fal fa-user-graduate',
+        label: 'Configurações'
+    },
 ];
