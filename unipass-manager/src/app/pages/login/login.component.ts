eed6456b572229bc614e7863eaa2be8cb44e6eab
vtrnduda <vtrnduda@gmail.com>
2025-12-09 22:01:57 -0300

    feat: cria componente de tela de login

---
+++ unipass-manager/src/app/pages/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [FormsModule]
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    // Simulação de autenticação bem-sucedida
+    localStorage.setItem('authToken', 'example-token');
+    this.router.navigate(['/inicio']);
+  }
+}
